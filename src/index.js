01d120a813e0de927fa1113b91fef2e3286c55c4
mathieuprt <mathieu.perrot06@gmail.com>
2023-09-07 15:06:58 +0200

    Ajout du Componant Heading + Css Extract

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import HelloWorldButton from "./componants/hello-world-button/hello-world-button.js";
+import Heading from "./componants/heading/heading.js";
 
 const helloWorldButton = new HelloWorldButton();
 helloWorldButton.render();
+
+const heading = new Heading();
+heading.render();
