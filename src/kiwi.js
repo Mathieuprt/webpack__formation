a799e47900c682adfb9d270823fbb99232027f01
mathieuprt <mathieu.perrot06@gmail.com>
2023-09-08 20:45:48 +0200

    Ajout KiwiImage + 2eme bundle

---
+++ src/kiwi.js
@@ -0,0 +1,9 @@
+import Heading from "./componants/heading/heading.js";
+
+import KiwiImage from "./componants/kiwi-image/kiwi-image.js";
+
+const heading = new Heading();
+heading.render();
+
+const kiwiImg = new KiwiImage();
+kiwiImg.render();
